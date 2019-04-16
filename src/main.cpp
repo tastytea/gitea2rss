d4fa51bcd951ea16e4b5e35b9a2b26d5696369af
tastytea <tastytea@tastytea.de>
2019-04-17 01:00:33 +0200

    Dump JSON to stdout.

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,59 @@
  */
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+#include <cstdint>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+#include <curlpp/Infos.hpp>
+#include "version.hpp"
 
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
+using std::ostringstream;
+using std::uint16_t;
+
+namespace curlopts = curlpp::options;
+
+const string get_http(const string &url)
+{
+    string answer;
+
+    try
+    {
+        ostringstream oss;
+        curlpp::Easy request;
+        request.setOpt<curlopts::Url>(url);
+        request.setOpt<curlopts::UserAgent>(string("gitea2rss/")
+                                            + global::version);
+        request.setOpt<curlopts::HttpHeader>({ "Connection: close" });
+        request.setOpt<curlopts::FollowLocation>(true);
+        request.setOpt<curlopts::WriteStream>(&oss);
+        request.perform();
+        uint16_t ret = curlpp::infos::ResponseCode::get(request);
+        if (ret == 200 || ret == 302 || ret == 307
+            || ret == 301 || ret == 308)
+        {
+            answer = oss.str();
+        }
+        else
+        {
+            cerr << "HTTP error: " << std::to_string(ret) << endl;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return answer;
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,5 +76,15 @@ int main(int argc, char *argv[])
         cerr << "usage: " << argv[0] << " URL of Gitea project\n";
         return 1;
     }
+
+    curlpp::initialize();
+
+    string url = argv[1];
+    size_t pos_repo = url.find('/', 8) + 1;
+    const string baseurl = url.substr(0, pos_repo - 1);
+    const string repo = url.substr(pos_repo);
+
+    cout << get_http(baseurl + "/api/v1/repos/" + repo + "/releases");
+
     return 0;
 }
