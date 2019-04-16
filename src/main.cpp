9a4d40a79d0fa7b8d56b7a0a6f2791297b962b08
tastytea <tastytea@tastytea.de>
2019-04-17 00:17:26 +0200

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/*  This file is part of gitea2rss.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cerr << "usage: " << argv[0] << " URL of Gitea project\n";
+        return 1;
+    }
+    return 0;
+}
