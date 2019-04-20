97c7e20da79e533880747441a58dbf1bb91b674c
tastytea <tastytea@tastytea.de>
2019-04-20 13:01:58 +0200

    Added tests for strings.cpp.
    get_baseurl(), get_domain(), get_repo(), get_project().

---
+++ tests/test_get_baseurl.cpp
@@ -0,0 +1,57 @@
+/*  This file is part of gitea2rss.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <catch.hpp>
+#include "gitea2rss.hpp"
+
+using std::string;
+
+SCENARIO ("get_baseurl() works as expected", "[strings]")
+{
+    WHEN ("HTTPS URL")
+    {
+        const string baseurl
+            = get_baseurl("https://git.example.com/user/project");
+
+        THEN ("The base URL is correctly returned")
+        {
+            REQUIRE(baseurl == "https://git.example.com");
+        }
+    }
+
+    WHEN ("HTTP URL")
+    {
+        const string baseurl
+            = get_baseurl("http://git.example.com/user/project");
+
+        THEN ("The base URL is correctly returned")
+        {
+            REQUIRE(baseurl == "http://git.example.com");
+        }
+    }
+
+    WHEN ("Gopher URL")
+    {
+        const string baseurl
+            = get_baseurl("gopher://git.example.com/user/project");
+
+        THEN ("The base URL is not correctly returned")
+        {
+            REQUIRE(baseurl == "gopher:/");
+        }
+    }
+}
---
+++ tests/test_get_domain.cpp
@@ -0,0 +1,35 @@
+/*  This file is part of gitea2rss.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <catch.hpp>
+#include "gitea2rss.hpp"
+
+using std::string;
+
+SCENARIO ("get_domain() works as expected", "[strings]")
+{
+    WHEN ("HTTPS URL is valid")
+    {
+        const string domain
+            = get_domain("https://git.example.com/user/project");
+
+        THEN ("The domain is correctly returned")
+        {
+            REQUIRE(domain == "git.example.com");
+        }
+    }
+}
