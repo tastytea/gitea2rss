d41b18183e62883bd453f409a7cd942431685b08
tastytea <tastytea@tastytea.de>
2019-04-21 10:52:36 +0200

    Added tests for get_http().

---
+++ tests/test_get_http.cpp
@@ -0,0 +1,44 @@
+/*  This file is part of gitea2rss.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <catch.hpp>
+#include "gitea2rss.hpp"
+
+using std::string;
+
+SCENARIO ("get_http() works as expected", "[http]")
+{
+    WHEN ("URL is valid")
+    {
+        const string answer = get_http("https://ip.tastytea.de/");
+
+        THEN ("Answer is not empty")
+        {
+            REQUIRE(answer != "");
+        }
+    }
+
+    WHEN ("URL is invalid")
+    {
+        const string answer = get_http("https://url.invalid/");
+
+        THEN ("Answer is empty")
+        {
+            REQUIRE(answer == "");
+        }
+    }
+}
